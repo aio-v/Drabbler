8af02e491feb259922eb155ffbab06d18e1b7757
Yvonne Hoang <vonne.hoang@gmail.com>
2022-03-04 01:13:23 -0800

    change logo color with theme

--- components/header/logo.jsx
+++ components/header/logo.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
-import { Typography } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 
 export function Logo() {
+    const theme = useTheme();
+
     return (
-        <Typography variant="h4" sx={{fontFamily:'Comfortaa', fontWeight:700,}}>
+        <Typography variant="h4" sx={{
+            fontFamily:'Comfortaa', 
+            fontWeight:700, 
+            color: theme.palette.mode === 'dark' ? 'primary.main' : 'secondary.main'}}>
             <Link href="/"><a>drabbler</a></Link>
         </Typography>
     )
